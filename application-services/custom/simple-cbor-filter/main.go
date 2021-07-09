8bfd1e88ca816eab194de27db239e605b9f7ee84
lenny <leonard.goodell@intel.com>
2021-07-09 10:58:31 -0700

    feat: Migrate simple-cbor-filter to V2
    applies to #38
    
    Signed-off-by: lenny <leonard.goodell@intel.com>

--- application-services/custom/simple-cbor-filter/main.go
+++ application-services/custom/simple-cbor-filter/main.go
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2019 Intel Corporation
+// Copyright (c) 2021 Intel Corporation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -25,49 +25,52 @@ import (
 	_ "image/png"
 	"os"
 
-	"github.com/edgexfoundry/go-mod-core-contracts/models"
-
-	"github.com/edgexfoundry/app-functions-sdk-go/appcontext"
-	"github.com/edgexfoundry/app-functions-sdk-go/appsdk"
-	"github.com/edgexfoundry/app-functions-sdk-go/pkg/transforms"
+	"github.com/edgexfoundry/app-functions-sdk-go/v2/pkg"
+	"github.com/edgexfoundry/app-functions-sdk-go/v2/pkg/interfaces"
+	"github.com/edgexfoundry/app-functions-sdk-go/v2/pkg/transforms"
+	"github.com/edgexfoundry/go-mod-core-contracts/v2/dtos"
 )
 
 const (
-	serviceKey = "sampleCborFilter"
+	serviceKey = "app-sample-cbor-filter"
 )
-
-var counter int = 0
 
 func main() {
 	// turn off secure mode for examples. Not recommended for production
-	os.Setenv("EDGEX_SECURITY_SECRET_STORE", "false")
-	// 1) First thing to do is to create an instance of the EdgeX SDK and initialize it.
-	edgexSdk := &appsdk.AppFunctionsSDK{ServiceKey: serviceKey}
-	if err := edgexSdk.Initialize(); err != nil {
-		edgexSdk.LoggingClient.Error(fmt.Sprintf("SDK initialization failed: %v\n", err))
+	_ = os.Setenv("EDGEX_SECURITY_SECRET_STORE", "false")
+
+	// 1) First thing to do is to create an new instance of an EdgeX Application Service.
+	service, ok := pkg.NewAppService(serviceKey)
+	if !ok {
 		os.Exit(-1)
 	}
 
+	// Leverage the built in logging service in EdgeX
+	lc := service.LoggingClient()
+
 	// 2) shows how to access the application's specific configuration settings.
-	valueDescriptors, err := edgexSdk.GetAppSettingStrings("ValueDescriptors")
+	resourceNames, err := service.GetAppSettingStrings("ResourceNames")
 	if err != nil {
-		edgexSdk.LoggingClient.Error(err.Error())
+		lc.Error(err.Error())
 		os.Exit(-1)
 	}
-	edgexSdk.LoggingClient.Info(fmt.Sprintf("Filtering for ValueDescriptors %v", valueDescriptors))
+	lc.Info(fmt.Sprintf("Filtering for ResourceNames %v", resourceNames))
 
 	// 3) This is our pipeline configuration, the collection of functions to
 	// execute every time an event is triggered.
-	edgexSdk.SetFunctionsPipeline(
-		transforms.NewFilter(valueDescriptors).FilterByValueDescriptor,
+	if err := service.SetFunctionsPipeline(
+		transforms.NewFilterFor(resourceNames).FilterByResourceName,
 		processImages,
-	)
+	); err != nil {
+		lc.Error("SetFunctionsPipeline returned error: ", err.Error())
+		os.Exit(-1)
+	}
 
 	// 4) Lastly, we'll go ahead and tell the SDK to "start" and begin listening for events
 	// to trigger the pipeline.
-	err = edgexSdk.MakeItRun()
+	err = service.MakeItRun()
 	if err != nil {
-		edgexSdk.LoggingClient.Error("MakeItRun returned error: ", err.Error())
+		lc.Error("MakeItRun returned error: ", err.Error())
 		os.Exit(-1)
 	}
 
@@ -76,16 +79,17 @@ func main() {
 	os.Exit(0)
 }
 
-func processImages(edgexcontext *appcontext.Context, params ...interface{}) (bool, interface{}) {
-	if len(params) < 1 {
-		// We didn't receive a result
-		return false, nil
+func processImages(ctx interfaces.AppFunctionContext, data interface{}) (bool, interface{}) {
+	// Leverage the built in logging service in EdgeX
+	lc := ctx.LoggingClient()
+
+	if data == nil {
+		return false, errors.New("processImages: No data received")
 	}
 
-	event, ok := params[0].(models.Event)
+	event, ok := data.(dtos.Event)
 	if !ok {
-		return false, errors.New("processImages didn't receive expect models.Event type")
-
+		return false, errors.New("processImages: didn't receive expect Event type")
 	}
 
 	for _, reading := range event.Readings {
@@ -93,12 +97,12 @@ func processImages(edgexcontext *appcontext.Context, params ...interface{}) (boo
 		imageData, imageType, err := image.Decode(bytes.NewReader(reading.BinaryValue))
 
 		if err != nil {
-			return false, errors.New("unable to decode image: " + err.Error())
+			return false, errors.New("processImages: unable to decode image: " + err.Error())
 		}
 
 		// Since this is a example, we will just print put some stats from the images received
-		fmt.Printf("Received Image from Device: %s, ReadingName: %s, Image Type: %s, Image Size: %s, Color in middle: %v\n",
-			reading.Device, reading.Name, imageType, imageData.Bounds().Size().String(),
+		lc.Infof("Received Image from Device: %s, ResourceName: %s, Image Type: %s, Image Size: %s, Color in middle: %v\n",
+			reading.DeviceName, reading.ResourceName, imageType, imageData.Bounds().Size().String(),
 			imageData.At(imageData.Bounds().Size().X/2, imageData.Bounds().Size().Y/2))
 	}
 
