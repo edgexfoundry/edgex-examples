202cfbb4593a5c77930812a095608fa09530bb71
Alex Ullrich <alex.ullrich@gmail.com>
2021-07-12 18:44:39 -0400

    feat(app): Update Secret Example to v2 SDK
    Update secrets example to released v2.0.0 SDK
    
    Signed-off-by: Alex Ullrich <alex.ullrich@gmail.com>

--- application-services/custom/secrets/main.go
+++ application-services/custom/secrets/main.go
@@ -1,5 +1,7 @@
 //
 // Copyright (c) 2020 Intel Corporation
+// Copyright (c) 2021 One Track Consulting
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -16,51 +18,54 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
-	"github.com/edgexfoundry/app-functions-sdk-go/appsdk"
-	"github.com/edgexfoundry/app-functions-sdk-go/pkg/transforms"
+	"github.com/edgexfoundry/app-functions-sdk-go/v2/pkg"
+	"github.com/edgexfoundry/app-functions-sdk-go/v2/pkg/transforms"
 
 	"secrets/functions"
 )
 
 const (
-	serviceKey = "secrets-example"
+	serviceKey = "app-secrets"
 )
 
 var counter int
 
 func main() {
 
-	// 1) First thing to do is to create an instance of the EdgeX SDK and initialize it.
-	edgexSdk := &appsdk.AppFunctionsSDK{ServiceKey: serviceKey}
-	if err := edgexSdk.Initialize(); err != nil {
-		edgexSdk.LoggingClient.Error(fmt.Sprintf("SDK initialization failed: %v\n", err))
+	// 1) First thing to do is to create an instance of the app service and initialize it.
+	appService, ok := pkg.NewAppService(serviceKey)
+	if !ok {
+		appService.LoggingClient().Error("SDK initialization failed.")
 		os.Exit(-1)
 	}
 
 	// 2) shows how to access the application's specific configuration settings.
-	deviceNames, err := edgexSdk.GetAppSettingStrings("DeviceNames")
+	deviceNames, err := appService.GetAppSettingStrings("DeviceNames")
 	if err != nil {
-		edgexSdk.LoggingClient.Error(err.Error())
+		appService.LoggingClient().Error(err.Error())
 		os.Exit(-1)
 	}
-	edgexSdk.LoggingClient.Info(fmt.Sprintf("Filtering for devices %v", deviceNames))
+	appService.LoggingClient().Infof("Filtering for devices %v", deviceNames)
 
 	// 3) This is our pipeline configuration, the collection of functions to
 	// execute every time an event is triggered.
-	edgexSdk.SetFunctionsPipeline(
-		transforms.NewFilter(deviceNames).FilterByDeviceName,
+	err = appService.SetFunctionsPipeline(
+		transforms.NewFilterFor(deviceNames).FilterByDeviceName,
 		transforms.NewConversion().TransformToXML,
 		functions.GetSecretsToConsole,
 	)
 
-	// 4) Lastly, we'll go ahead and tell the SDK to "start" and begin listening for events
-	// to trigger the pipeline.
-	err = edgexSdk.MakeItRun()
 	if err != nil {
-		edgexSdk.LoggingClient.Error("MakeItRun returned error: ", err.Error())
+		appService.LoggingClient().Errorf("SetFunctionsPipeline returned error: %s", err.Error())
+	}
+
+	// 4) Lastly, we'll go ahead and tell the service to "start" and begin listening for events
+	// to trigger the pipeline.
+	err = appService.MakeItRun()
+	if err != nil {
+		appService.LoggingClient().Errorf("MakeItRun returned error: %s", err.Error())
 		os.Exit(-1)
 	}
 
